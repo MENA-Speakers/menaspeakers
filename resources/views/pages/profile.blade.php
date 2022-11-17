761589d4a9a67c6259cfade3c9145215b69fcdcc
Joshua Fomubod <joshuafomum@gmail.com>
2022-11-17 19:06:47 +0400

    remove zoompim

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -30,16 +30,16 @@
       formId: "fb8b0ab0-0c41-4a33-b8ed-5d4ee5cc4ed4"
     });
   </script>
-  <!--Start of Zopim Live Chat Script-->
-  <script type="text/javascript">
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-      z._.push(c)},$=z.s=
-      d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-      $.src='//v2.zopim.com/?4A45ojPv2RHn1Vm4zNxg4qPM8jn9a3I2';z.t=+new Date;$.
-        type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-  </script>
-  <!--End of Zopim Live Chat Script-->
+{{--  <!--Start of Zopim Live Chat Script-->--}}
+{{--  <script type="text/javascript">--}}
+{{--    window.$zopim||(function(d,s){var z=$zopim=function(c){--}}
+{{--      z._.push(c)},$=z.s=--}}
+{{--      d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.--}}
+{{--    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');--}}
+{{--      $.src='//v2.zopim.com/?4A45ojPv2RHn1Vm4zNxg4qPM8jn9a3I2';z.t=+new Date;$.--}}
+{{--        type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');--}}
+{{--  </script>--}}
+{{--  <!--End of Zopim Live Chat Script-->--}}
   <!-- Google Code for Remarketing Tag -->
   <script type="text/javascript">
     /* <![CDATA[ */
