ee9873e2707ae9c87e44b3d6d0411da6e0f7f56a
Joshua Fomubod <joshuafomum@gmail.com>
2023-04-07 11:24:29 +0400

    speaker show page layout clean up

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -46,7 +46,7 @@ export default function MainLayout({ children}) {
              ))}
            </div>
            <div className=" hidden lg:flex lg:flex-1 lg:justify-end">
-             <Link href="#" className="bg-gradient-to-r from-pink-500 to-rose-500 text-sm font-semibold py-2.5 px-4 py-2 px-4 text-white">
+             <Link href={route('pages.contact')} className="bg-gradient-to-r from-pink-500 to-rose-500 text-sm font-semibold py-2.5 px-4 py-2 px-4 text-white">
                Book Now <span aria-hidden="true">&rarr;</span>
              </Link>
            </div>
@@ -86,7 +86,7 @@ export default function MainLayout({ children}) {
                    ))}
                  </div>
                  <div className=" hidden lg:flex lg:flex-1 lg:justify-end">
-                   <Link href="#" className="bg-gradient-to-r from-pink-500 to-rose-500 text-sm font-semibold py-2.5 px-4 py-2 px-4 text-white">
+                   <Link href={route('pages.contact')} className="bg-gradient-to-r from-pink-500 to-rose-500 text-sm font-semibold py-2.5 px-4 py-2 px-4 text-white">
                      Book Now <span aria-hidden="true">&rarr;</span>
                    </Link>
                  </div>
--- resources/js/Pages/Speakers/Show.jsx
+++ resources/js/Pages/Speakers/Show.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MainLayout from "@/Layouts/MainLayout";
-import {Head} from "@inertiajs/react";
+import {Head, Link} from "@inertiajs/react";
 
 function Show({speaker}) {
   return (
@@ -13,24 +13,31 @@ function Show({speaker}) {
         <img src="https://images.unsplash.com/photo-1561489396-888724a1543d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29uZnJlbmNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" className="w-full h-full object-cover"/>
 
         <div className="absolute inset-0 z-0">
-          <div className="w-full h-full z-20 bg-rose-500/80">
+          <div className="w-full h-full z-20 bg-black/80">
             <div className="max-w-7xl mx-auto flex items-center justify-center h-full  px-4 sm px-6 lg px-8">
-              <h1 className="text-white text-5xl font-bold">{ speaker.name }</h1>
+              <h1 className="text-gray-200 text-5xl font-bold">{ speaker.name }</h1>
             </div>
           </div>
         </div>
       </section>
 
 
-      <section className={'p-12'}>
-        <div className="max-7xl mx-auto px-4 sm px-6 lg px-8">
-            <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
-              <div className="">
-                <img className={'w-full h-96 object-cover'} src={speaker.image} alt={speaker.name}/>
+      <section className={'p-6 lg:p-12'}>
+        <div className="max-7xl mx-auto px-4 sm lg:px-8">
+            <div className="grid grid-cols-2 lg:grid-cols-6 lg:w-[90%]">
+              <div className="col-span-2 lg:p-6">
+                <div className={'lg:w-[80%] space-y-6  flex flex-col items-center justify-center'}>
+                  <img className={'w-full h-96 object-cover'} src={'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'} alt={speaker.name}/>
+
+                {/*  Book Speaker*/}
+                  <Link href={route('pages.contact')} className="bg-gradient-to-r from-pink-500 to-rose-500  font-semibold py-3 px-4 py-2 px-4 text-white w-full">
+                    Book Speaker
+                  </Link>
+                </div>
 
               </div>
 
-              <div className="col-span-1 lg:col-span-5 p-8">
+              <div className="col-span-2 lg:col-span-4 p-8">
                 <div dangerouslySetInnerHTML={{__html: speaker.bio}}>
 
                 </div>
