b0bcdfa2c2ee833518e069546cea39a83231c183
Ahmed Abuwarda <ahmedaabuwarda@gmail.com>
2023-04-14 15:26:30 +0300

    update sitemap to add all gallery images

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Image;
 use App\Models\Blog;
 use App\Models\Speaker;
 use Illuminate\Console\Command;
@@ -35,10 +36,15 @@ class GenerateSitemap extends Command
     $sitemap = Sitemap::create()
       ->add(Url::create('/'))
       ->add(Url::create('/about'))
-      ->add(Url::create('/contact'))
+      ->add(Url::create('/speakers'))
+      ->add(Url::create('/gallery'))
+      ->add(Url::create('/blogs'))
+      ->add(Url::create('/page/contact'))
       ->add(Url::create('/terms'))
       ->add(Url::create('/privacy'))
-      ->add(Url::create('/refund'))
+      ->add(Url::create('/FAQs'))
+      ->add(Url::create('/page/refund-policy'))
+      ->add(Url::create('/page/contact'))
       ->add(Url::create('/profile'))
       ->add(Url::create('/profile-arabic'));
 
@@ -50,7 +56,7 @@ class GenerateSitemap extends Command
         $url = Url::create($image->getUrl())
           ->setLastModificationDate($speaker->updated_at)
           ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
-          ->setPriority(0.9);
+          ->setPriority(1);
         $sitemap->add($url);
       }
     });
@@ -58,6 +64,23 @@ class GenerateSitemap extends Command
     // Blogs
     Blog::all()->each(function ($blog) use ($sitemap) {
       $sitemap->add(Url::create('/blogs/' . $blog->slug));
+      $images = $blog->getMedia('image');
+      foreach ($images as $image) {
+        $url = Url::create($image->getUrl())
+          ->setLastModificationDate($blog->updated_at)
+          ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+          ->setPriority(0.9);
+        $sitemap->add($url);
+      }
+    });
+
+    // Get all gallery images
+    Image::all()->each(function ($image) use ($sitemap) {
+      $url = Url::create($image->getFirstMediaUrl('image'))
+        ->setLastModificationDate($image->updated_at)
+        ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+        ->setPriority(0.8);
+      $sitemap->add($url);
     });
 
     $sitemap->writeToFile(public_path('sitemap.xml'));
