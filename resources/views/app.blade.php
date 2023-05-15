b178592e6de8aae2b9901adad915a9d9c9be4d66
Ahmed Abuwarda <ahmedaabuwarda@gmail.com>
2023-05-15 16:11:44 +0300

    add meta data to mena

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,9 +9,28 @@
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,700;0,800;0,900;1,400&family=Poppins:wght@200;300;400;500;600;700&display=swap"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,700;0,800;0,900;1,400&family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+
+  <!-- meta -->
+  <meta name="title" content="Mena Speakers - Leading Provider of Professional Speakers">
+  <meta name="description" content="Mena Speakers is a leading provider of professional speakers for corporate events, conferences, and other occasions. We offer a wide range of speakers, including business leaders, celebrities, and experts in a variety of fields.">
+  <meta name="keywords" content="professional speakers, corporate events, conferences, speakers, business leaders, celebrities, experts, events, Dubai, United Arab Emirates">
+  <meta name="author" content="Mena Speakers">
+  <meta name="copyright" content="Copyright © 2023 Mena Speakers">
+  <meta name="robots" content="index, follow">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="og:title" content="Mena Speakers - Leading Provider of Professional Speakers">
+  <meta name="og:description" content="Mena Speakers is a leading provider of professional speakers for corporate events, conferences, and other occasions. We offer a wide range of speakers, including business leaders, celebrities, and experts in a variety of fields.">
+  <meta name="og:image" content="https://www.menaspeakers.com/images/logo.png">
+  <meta name="dc.title" content="Mena Speakers - Leading Provider of Professional Speakers">
+  <meta name="dc.description" content="Mena Speakers is a leading provider of professional speakers for corporate events, conferences, and other occasions. We offer a wide range of speakers, including business leaders, celebrities, and experts in a variety of fields.">
+  <meta name="dc.subject" content="professional speakers, corporate events, conferences, speakers, business leaders, celebrities, experts, events, Dubai, United Arab Emirates">
+  <meta name="dc.creator" content="Mena Speakers">
+  <meta name="dc.publisher" content="Mena Speakers">
+  <meta name="dc.date" content="2023-05-15">
+  <meta name="dc.type" content="website">
+  <meta name="dc.format" content="text/html">
+  <meta name="dc.language" content="en-US">
 
   <!-- Scripts -->
   @routes
@@ -18,7 +38,9 @@
   @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
   @inertiaHead
 </head>
+
 <body class="font-poppins antialiased">
-@inertia
+  @inertia
 </body>
+
 </html>
