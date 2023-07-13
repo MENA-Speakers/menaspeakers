927ec4a69ae334ae6b379bf7320b24509a3d1bf3
Joshua Fomubod <joshuafomum@gmail.com>
2023-07-13 14:25:50 +0400

    add validation for video upload

--- app/Http/Controllers/Admin/AdminSpeakerVideoController.php
+++ app/Http/Controllers/Admin/AdminSpeakerVideoController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\SpeakerResource;
 use App\Models\Speaker;
 use App\Models\Video;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 
 class AdminSpeakerVideoController extends Controller
@@ -20,6 +21,14 @@ class AdminSpeakerVideoController extends Controller
     ]);
   }
 
+
+  private function extractYouTubeId($url)
+  {
+    $pattern = '/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/';
+    preg_match($pattern, $url, $matches);
+    return isset($matches[1]) ? $matches[1] : null;
+  }
+
   public function store(Request $request, Speaker $speaker)
   {
     //Validate
@@ -28,9 +37,26 @@ class AdminSpeakerVideoController extends Controller
       'videoSource' => 'required',
     ]);
 
-    //extract video id
-    $videoId = $request->videoSource === 'youtube' ?
-      explode('=', $request->link)[1] : explode('.com/', $request->link)[1];
+    $videoId = null;
+
+    //if video is youtube extract the id
+    if($request->videoSource === 'youtube') {
+      $videoId = $this->extractYouTubeId($request->input('link'));
+      //throw error if link is not valid
+      if( strlen($videoId) !== 11 ) {
+        throw  ValidationException::withMessages([
+          'link' => 'Please enter a valid youtube link'
+        ]);
+      } else if($request->videoSource === 'vimeo') {
+        $videoId = explode('.com/', $request->link)[1];
+        if( strlen($videoId) !== 9 ) {
+          throw  ValidationException::withMessages([
+            'link' => 'Please enter a valid vimeo link'
+          ]);
+        }
+      }
+    }
+
     $speaker->videos()->create([
       'url' => $videoId
     ]);
