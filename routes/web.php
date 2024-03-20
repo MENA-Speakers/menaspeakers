0554e4accea5e863f2c2544607eeb9cc7ba47000
Joshua Fomubod <joshuafomum@gmail.com>
2024-03-20 10:18:24 +0400

    add proposal crud, update proposal preview

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,7 @@
   Route::get('contact', [PagesController::class, 'contact'])->name('pages.contact');
   Route::get('page/refund-policy', [PagesController::class, 'policy'])->name('pages.policy');
 
-  Route::get('api/proposals', [ApiProposalController::class, 'store'])->name('api.new-proposal');
+  Route::post('api/proposals', [ApiProposalController::class, 'store'])->name('api.new-proposal');
 
 
   //External site routes
