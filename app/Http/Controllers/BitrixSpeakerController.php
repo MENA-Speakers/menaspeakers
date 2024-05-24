86ba746306b3fd1b5e98e1d13b5b8cf4c6ee7a86
Joshua Fomubod <joshuafomum@gmail.com>
2024-05-24 13:15:43 +0400

    add bitrix speakers link

--- app/Http/Controllers/BitrixSpeakerController.php
+++ app/Http/Controllers/BitrixSpeakerController.php
@@ -10,19 +10,11 @@ class BitrixSpeakerController extends Controller
     public function store(Request $request): \Illuminate\Http\JsonResponse
     {
 
-      //extract first name from request full name
-      $name = explode(' ', $request->input('NAME'));
-
-      $last_name = (isset($name[1]) ? $name[1] : '') . ' ' .
-               (isset($name[2]) ? $name[2] : '') . ' ' .
-               (isset($name[3]) ? $name[3] : '');
-
 
       $profile = Profile::firstOrCreate([
         'bitrix_id' => $request->input('ID')],
         [
-          'first_name' => $name[0],
-          'last_name' => $last_name,
+          'name' => $request->input('NAME'),
           'email' => $request->input('EMAIL'),
           'bitrix_id' => $request->input('ID'),
         ]
