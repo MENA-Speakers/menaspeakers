9634397245aeb556e7b685e3bb1a63b0f840ce9e
Joshua Fomubod <joshuafomum@gmail.com>
2024-06-11 09:06:53 +0400

    added search filter

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -6,8 +6,10 @@ use App\Http\Resources\ProfileResource;
 use App\Http\Resources\SpeakerResource;
 use App\Imports\BlogImport;
 use App\Imports\SpeakerImport;
+use App\Models\Category;
 use App\Models\Location;
 use App\Models\Speaker;
+use App\Models\Topic;
 use Artesaos\SEOTools\Facades\JsonLdMulti;
 use Artesaos\SEOTools\Facades\SEOTools;
 use Illuminate\Http\Request;
@@ -32,12 +34,35 @@ class ProfilesController extends Controller
 
     }
 
-    $locations = Location::all();
+    $topics = Topic::all()->map(function($topic) {
+      return [
+        'value' => $topic->id,
+        'label' => $topic->name,
+      ];
+    });
+
+    $categories = Category::all()->map(function($category) {
+      return [
+        'value' => $category->id,
+        'label' => $category->name,
+      ];
+    });
+
+    $locations = Location::all()->map(function($location) {
+      return [
+        'value' => $location->id,
+        'label' => $location->name,
+      ];
+    });
+
+
 
     return Inertia::render('Speakers/Index', [
         'speakers' => SpeakerResource::collection($result),
         'query' => $request->input('query'),
-        'locations' => $locations
+        'locations' => $locations,
+        'topics' => $topics,
+        'categories' => $categories,
     ]);
   }
 
