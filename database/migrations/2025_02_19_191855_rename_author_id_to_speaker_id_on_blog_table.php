13699d7f63fc674aea63c5c4294e0baff7c169e7
Joshua Fomubod <joshuafomum@gmail.com>
2025-02-19 23:20:38 +0400

    add migration to rename author id to spekaer_id on blogs table

---
+++ database/migrations/2025_02_19_191855_rename_author_id_to_speaker_id_on_blog_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->renameColumn('author_id', 'speaker_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->renameColumn('speaker_id', 'author_id');
+        });
+    }
+};
