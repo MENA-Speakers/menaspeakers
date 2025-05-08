0806d9f15213f3fb64b52ff8fa28f05ec0994dc6
249Ayman <140142498+249Ayman@users.noreply.github.com>
2025-05-08 10:30:35 +0300

    added author to blog-card

--- resources/js/Components/BlogCard.tsx
+++ resources/js/Components/BlogCard.tsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React from "react";
 import { Link } from "@inertiajs/react";
 import truncateText from "@/Utils/truncateText";
 
 interface BlogCardProps {
-  blog: BlogType
+  blog: BlogType;
 }
-function BlogCard({ blog } : BlogCardProps) {
+function BlogCard({ blog }: BlogCardProps) {
   return (
     <div className="rounded-2xl overflow-hidden border">
-      <Link href={route('blogs.show', blog.slug)} aria-label={'Read blog details'}>
-        <img className={'h-60 w-full object-cover'}
-             src={blog.image}
-             alt={blog.title}/>
+      <Link
+        href={route("blogs.show", blog.slug)}
+        aria-label={"Read blog details"}
+      >
+        <img
+          className={"h-60 w-full object-cover"}
+          src={blog.image}
+          alt={blog.title}
+        />
       </Link>
-      <div className={'py-4 space-y-4 p-3'}>
-        <p className="text-xs py-1 text-slate-500">
-          {
-            blog.published_at
-          }
-        </p>
-        <Link className={''} href={route('blogs.show', blog.slug)} aria-label={blog.title}>{blog.title}</Link>
-
+      <div className={"py-4 space-y-4 p-3"}>
+        <div className="flex justify-between items-center">
+          <p className="text-xs py-1 text-slate-500">{blog.published_at}</p>
+          <p className="text-xs py-1 text-slate-500">
+            By {blog.author?.name || "MENA Speakers"}
+          </p>
+        </div>
+        <Link
+          className={""}
+          href={route("blogs.show", blog.slug)}
+          aria-label={blog.title}
+        >
+          {blog.title}
+        </Link>
       </div>
-
     </div>
   );
 }
